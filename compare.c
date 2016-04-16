1384261a0952c5c3ac21e93ef9799292c6023a2e
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-04-16 02:12:56 -0300

    version final compare anexo a pcml

--- compare.c
+++ compare.c
@@ -3,43 +3,34 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int compare(char *Str1, char * Str2, int corto, int *pos_dif){
-    int half = corto/2;
-    if (half == 0){
-        if (Str1[0] == Str2[0]){
-            return 0;
-        }
-        else{
+
+int diferentes(char *Str1, char * Str2, int largo, int *pos_dif) {      //comparar 2 strings y guardar la pos en que son diferentes
+    int half = largo / 2;
+    if (Str1[half] != Str2[half]) {                                     //buscar diferencia en mitad
+        *pos_dif = half;
+    }
+    if (largo == 1) {                                                   //case base
+        if (Str1[0] != Str2[0]) {
             *pos_dif = 0;
             return 1;
         }
+        else {
+            return 0;
+        }
     }
-    if (Str1[half] != Str2[half]){
-        *pos_dif = half;
+    if (diferentes(Str1, Str2, half, pos_dif) == 0) {                   //comparar recursivamente en cada mitad
+        int i;                                                          //de no encontrar, buscar en mitad de adelante
+        for (i = half+1; i < largo; i++) {
+            if (Str1[i] != Str2[i]) {                                   //encontro distinto
+                *pos_dif = i;
+                return 1;
+            }
+        }
+        if (*pos_dif == largo){                                         //comprobar si hay distinto encontrado en comparacion de mitades
+            return 1;
+        }
+        return 0;
     }
-    if (comparacion){
-        compare(Str1, Str2, half, pos_dif);
-    }
-    if (*pos_dif == -1 && half>1){
-        half += half/2;
-        compare(Str1, Str2, half, pos_dif);
-    }
-}
-
-int main(){
-    char *pal1 = "hola";
-    char *pal2 = "holgura";
-    int min;
-    if (strlen(pal1)<strlen(pal2)){
-        min = strlen(pal1);
-    }
-    else{
-        min = strlen(pal2);
-    }
-    int pos = -1;
-    int dif = compare(pal1, pal2, min, &pos);
-    printf("Son dif?: %d en %d\n", dif, pos);
 }
